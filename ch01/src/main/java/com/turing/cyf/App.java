037cab6bfbb19292266837b3fdb5a8e3bccc9991
cyf <cyf@cyfdeMacBook-Pro.local>
2017-07-24 14:32:06 +0800

    创建第一个scala项目

---
+++ ch01/src/main/java/com/turing/cyf/App.java
@@ -0,0 +1,13 @@
+package com.turing.cyf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
